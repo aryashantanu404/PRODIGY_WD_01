4302647f1aea89b605db0d7800ecf02697a8e7ec
Arya Shantanu <aryas.ug23.ec@nitp.ac.in>
2024-08-11 19:21:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//swiper slider//
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 0,
+  });
+  var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    
+    thumbs: {
+      swiper: swiper,
+    },
+  });
